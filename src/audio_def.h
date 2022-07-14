861437f5abef002f4f11f01264144fbbedbb89c7
UNSTOP4BLE <bruno.juraski1@gmail.com>
2022-07-14 22:10:41 +0200

    Initial commit

---
+++ src/audio_def.h
@@ -0,0 +1,75 @@
+#define XA_LENGTH(x) (((u64)(x) * 75) / 100 * IO_SECT_SIZE) //Centiseconds to sectors in bytes (w)
+
+typedef struct
+{
+	XA_File file;
+	u32 length;
+} XA_TrackDef;
+
+static const XA_TrackDef xa_tracks[] = {
+	//MENU.XA
+	{XA_Menu, XA_LENGTH(11300)}, //XA_GettinFreaky
+	{XA_Menu, XA_LENGTH(3800)},  //XA_GameOver
+	//WEEK1A.XA
+	{XA_Week1A, XA_LENGTH(7700)}, //XA_Bopeebo
+	{XA_Week1A, XA_LENGTH(8000)}, //XA_Fresh
+	//WEEK1B.XA
+	{XA_Week1B, XA_LENGTH(8700)}, //XA_Dadbattle
+	{XA_Week1B, XA_LENGTH(6800)}, //XA_Tutorial
+	//WEEK2A.XA
+	{XA_Week2A, XA_LENGTH(9900)}, //XA_Spookeez
+	{XA_Week2A, XA_LENGTH(8900)}, //XA_South
+	//WEEK2B.XA
+	{XA_Week2B, XA_LENGTH(17800)}, //XA_Monster
+	//WEEK3A.XA
+	{XA_Week3A, XA_LENGTH(8400)},  //XA_Pico
+	{XA_Week3A, XA_LENGTH(10000)}, //XA_Philly
+	//WEEK3B.XA
+	{XA_Week3B, XA_LENGTH(10700)}, //XA_Blammed
+	//WEEK4A.XA
+	{XA_Week4A, XA_LENGTH(9300)},  //XA_SatinPanties
+};
+
+static const char *xa_paths[] = {
+	"\\MUSIC\\MENU.XA;1",   //XA_Menu
+	"\\MUSIC\\WEEK1A.XA;1", //XA_Week1A
+	"\\MUSIC\\WEEK1B.XA;1", //XA_Week1B
+	"\\MUSIC\\WEEK2A.XA;1", //XA_Week2A
+	"\\MUSIC\\WEEK2B.XA;1", //XA_Week2B
+	"\\MUSIC\\WEEK3A.XA;1", //XA_Week3A
+	"\\MUSIC\\WEEK3B.XA;1", //XA_Week3B
+	"\\MUSIC\\WEEK4A.XA;1", //XA_Week4A
+	NULL,
+};
+
+typedef struct
+{
+	const char *name;
+	boolean vocal;
+} XA_Mp3;
+
+static const XA_Mp3 xa_mp3s[] = {
+	//MENU.XA
+	{"freaky", false},   //XA_GettinFreaky
+	{"gameover", false}, //XA_GameOver
+	//WEEK1A.XA
+	{"bopeebo", true}, //XA_Bopeebo
+	{"fresh", true},   //XA_Fresh
+	//WEEK1B.XA
+	{"dadbattle", true}, //XA_Dadbattle
+	{"tutorial", false}, //XA_Tutorial
+	//WEEK2A.XA
+	{"spookeez", true}, //XA_Spookeez
+	{"south", true},    //XA_South
+	//WEEK2B.XA
+	{"monster", true}, //XA_Monster
+	//WEEK3A.XA
+	{"pico", true},   //XA_Pico
+	{"philly", true}, //XA_Philly
+	//WEEK3B.XA
+	{"blammed", true}, //XA_Blammed
+	//WEEK4A.XA
+	{"satinpanties", true}, //XA_SatinPanties
+	
+	{NULL, false}
+};
---
+++ src/stagedef_disc1.h
@@ -0,0 +1,174 @@
+	{ //StageId_1_1 (talentless fox)
+		//Characters
+		{Char_BF_New,    FIXED_DEC(60,1),  FIXED_DEC(100,1)},
+		{Char_Dad_New, FIXED_DEC(-120,1),  FIXED_DEC(100,1)},
+        {NULL},
+		{Char_GF_New,     FIXED_DEC(0,1),  FIXED_DEC(-10,1)},
+		
+		//Stage background
+		Back_Week1_New,
+		
+		//Song info
+		{FIXED_DEC(1,1),FIXED_DEC(29,10),FIXED_DEC(1,1)},
+		1, 1,
+		XA_Bopeebo, 0,
+		
+		StageId_1_2, STAGE_LOAD_FLAG
+	},
+	{ //StageId_1_2 (no-villains)
+		//Characters
+		{Char_BF_New,    FIXED_DEC(60,1),  FIXED_DEC(100,1)},
+		{Char_Dad_New, FIXED_DEC(-120,1),  FIXED_DEC(100,1)},
+        {NULL},
+		{Char_GF_New,     FIXED_DEC(0,1),  FIXED_DEC(-10,1)},
+		
+		//Stage background
+		Back_Week1_New,
+		
+		//Song info
+		{FIXED_DEC(1,1),FIXED_DEC(31,10),FIXED_DEC(1,1)},
+		1, 2,
+		XA_Fresh, 2,
+		
+		StageId_1_3, STAGE_LOAD_FLAG
+	},
+	{ //StageId_1_3 (die-batsards)
+		//Characters
+		{Char_BF_New,    FIXED_DEC(60,1),  FIXED_DEC(100,1)},
+		{Char_Dad_New, FIXED_DEC(-120,1),  FIXED_DEC(100,1)},
+        {NULL},
+		{Char_GF_New,     FIXED_DEC(0,1),  FIXED_DEC(-10,1)},
+		
+		//Stage background
+		Back_Week1_New,
+		
+		//Song info
+		{FIXED_DEC(1,1),FIXED_DEC(3,1),FIXED_DEC(1,1)},
+		1, 3,
+		XA_Dadbattle, 0,
+		
+		StageId_1_3, 0
+	},
+	
+	{ //StageId_2_1 (high-shovel)
+		//Characters
+		{Char_BF_New,      FIXED_DEC(56,1),   FIXED_DEC(85,1)},
+		{Char_Spook_New,  FIXED_DEC(-90,1),   FIXED_DEC(85,1)},
+        {NULL},
+		{Char_GF_New,       FIXED_DEC(0,1),  FIXED_DEC(-15,1)},
+		
+		//Stage background
+		Back_Week2_New,
+		
+		//Song info
+		{FIXED_DEC(1,1),FIXED_DEC(27,10),FIXED_DEC(1,1)},
+		2, 1,
+		XA_Spookeez, 0,
+		
+		StageId_2_2, STAGE_LOAD_FLAG
+	},
+	{ //StageId_2_2 (taste-for-blood)
+		//Characters
+		{Char_BF_New,      FIXED_DEC(56,1),   FIXED_DEC(85,1)},
+		{Char_Spook_New,  FIXED_DEC(-90,1),   FIXED_DEC(85,1)},
+        {NULL},
+		{Char_GF_New,       FIXED_DEC(0,1),  FIXED_DEC(-15,1)},
+		
+		//Stage background
+		Back_Week2_New,
+		
+		//Song info
+		{FIXED_DEC(1,1),FIXED_DEC(35,10),FIXED_DEC(1,1)},
+		2, 2,
+		XA_South, 2,
+		
+		StageId_2_3, STAGE_LOAD_FLAG | STAGE_LOAD_OPPONENT
+	},
+	{ //StageId_2_3 (tsuraran-fox)
+		//Characters
+		{Char_BF_New,      FIXED_DEC(56,1),   FIXED_DEC(85,1)},
+		{Char_Monster_New,  FIXED_DEC(-90,1),   FIXED_DEC(85,1)},
+        {NULL},
+		{Char_GF_New,       FIXED_DEC(0,1),  FIXED_DEC(-15,1)},
+		
+		//Stage background
+		Back_Week2_New,
+		
+		//Song info
+		{FIXED_DEC(1,1),FIXED_DEC(29,10),FIXED_DEC(1,1)},
+		2, 3,
+		XA_Monster, 0,
+		
+		StageId_2_3, 0
+	},
+	
+	{ //StageId_3_1 (no-heroes)
+		//Characters
+		{Char_BF_New,     FIXED_DEC(56,1),   FIXED_DEC(85,1)},
+		{Char_Pico_New, FIXED_DEC(-105,1),   FIXED_DEC(85,1)},
+        {NULL},
+		{Char_GF_New,      FIXED_DEC(0,1),  FIXED_DEC(-15,1)},
+		
+		//Stage background
+		Back_Week3_New,
+		
+		//Song info
+		{FIXED_DEC(1,1),FIXED_DEC(32,10),FIXED_DEC(1,1)},
+		3, 1,
+		XA_Pico, 0,
+		
+		StageId_3_2, STAGE_LOAD_FLAG
+	},
+	{ //StageId_3_2 (no-bitches-matasaki)
+		//Characters
+		{Char_BF_New,     FIXED_DEC(56,1),   FIXED_DEC(85,1)},
+		{Char_Pico_New, FIXED_DEC(-105,1),   FIXED_DEC(85,1)},
+        {NULL},
+		{Char_GF_New,      FIXED_DEC(0,1),  FIXED_DEC(-15,1)},
+		
+		//Stage background
+		Back_Week3_New,
+		
+		//Song info
+		{FIXED_DEC(1,1),FIXED_DEC(31,10),FIXED_DEC(1,1)},
+		3, 2,
+		XA_Philly, 2,
+		
+		StageId_3_3, STAGE_LOAD_FLAG
+	},
+	{ //StageId_3_3 (no-bitches-penkaru)
+		//Characters
+		{Char_BF_New,     FIXED_DEC(56,1),   FIXED_DEC(85,1)},
+		{Char_Pico_New, FIXED_DEC(-105,1),   FIXED_DEC(85,1)},
+        {NULL},
+		{Char_GF_New,      FIXED_DEC(0,1),  FIXED_DEC(-15,1)},
+		
+		//Stage background
+		Back_Week3_New,
+		
+		//Song info
+		{FIXED_DEC(1,1),FIXED_DEC(31,10),FIXED_DEC(1,1)},
+		3, 3,
+		XA_Blammed, 0,
+		
+		StageId_3_3, 0
+	},
+	
+	{ //StageId_4_1 (groovy-fox)
+		//Characters
+		{Char_BF_New,   FIXED_DEC(120,1),   FIXED_DEC(40,1)},
+		{Char_Mom_New, FIXED_DEC(-120,1),  FIXED_DEC(100,1)},
+        {NULL},
+		{Char_GF_New,    FIXED_DEC(-6,1),   FIXED_DEC(-5,1)},
+		
+		//Stage background
+		Back_Week4_New,
+		
+		//Song info
+		{FIXED_DEC(1,1),FIXED_DEC(25,10),FIXED_DEC(1,1)},
+		4, 1,
+		XA_SatinPanties, 0,
+		
+		StageId_4_1, 0
+	},
+	
