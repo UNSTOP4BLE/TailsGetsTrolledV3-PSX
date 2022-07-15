d94df0ea1b75db57ec3982dbf66bb91a546fd2a3
UNSTOP4BLE <88965856+UNSTOP4BLE@users.noreply.github.com>
2022-07-15 03:30:21 +0200

    grass lol

--- src/stage/week1.c
+++ src/stage/week1.c
@@ -21,6 +21,22 @@ typedef struct
 	Gfx_Tex tex_back1; //Curtains
 } Back_Week1;
 
+static void DrawGrass(Gfx_Tex tex, RECT_FIXED grass_dst)
+{
+	RECT grass_src = {136, 0, 119, 11};
+	Stage_DrawTex(&tex, &grass_src, &grass_dst, stage.camera.bzoom);
+	grass_src.y += 11;
+	grass_dst.x += grass_dst.w;
+	Stage_DrawTex(&tex, &grass_src, &grass_dst, stage.camera.bzoom);
+	grass_src.y += 11;
+	grass_dst.x += grass_dst.w;
+	Stage_DrawTex(&tex, &grass_src, &grass_dst, stage.camera.bzoom);
+
+	RECT green_fill = {152, 6, 1, 1};
+	RECT_FIXED green_dst = {FIXED_DEC(-screen.SCREEN_WIDTH / 2,1), grass_dst.y + FIXED_DEC(11,1), FIXED_DEC(screen.SCREEN_WIDTH,1), FIXED_DEC(120,1)};
+	Stage_DrawTex(&tex, &green_fill, &green_dst, stage.camera.bzoom);
+}
+
 //Week 1 background functions
 void Back_Week1_DrawBG(StageBack *back)
 {
@@ -31,6 +47,10 @@ void Back_Week1_DrawBG(StageBack *back)
 	//Draw curtains
 	fx = stage.camera.x;
 	fy = stage.camera.y;
+
+	RECT_FIXED grass_dst = {FIXED_DEC(-229,1) - fx, FIXED_DEC(44,1) - fy, FIXED_DEC(205,1), FIXED_DEC(11,1)}; 
+	Debug_StageMoveDebug(&grass_dst,7,fx,fy);
+	DrawGrass(this->tex_back0, grass_dst);
 
 	RECT tree_src = {0, 0, 74, 256};
 	RECT_FIXED tree_dst = {FIXED_DEC(0,1) - fx, FIXED_DEC(0,1) - fy, FIXED_DEC(tree_src.w,1), FIXED_DEC(tree_src.h,1)}; 
